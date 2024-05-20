67e594294e8ec94e76586ab751cf20f192e0b6c4
Ploux <peter@humo.golf>
2024-05-20 14:15:28 -0500

    Initial Commit

---
+++ LoginSignup/LoginSignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSignupApp.swift
+//  LoginSignup
+//
+//  Created by Peter Loux on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginSignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
